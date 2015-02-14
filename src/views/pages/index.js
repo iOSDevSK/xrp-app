a0309fb9b84384aadb9a4aa85b63e71c5b44d0c9
Adam Cmiel <adam@famo.us>
2015-02-14 13:35:53 -0800

    add the flash to the exported pages

--- src/views/pages/index.js
+++ src/views/pages/index.js
@@ -1,6 +1,7 @@
 import HomeView from './HomeView'
 import InfoView from './InfoView'
+import FlashView from './FlashView'
 import SendPaymentsView from './SendPaymentsView'
 
-export {HomeView, InfoView, SendPaymentsView}
+export {HomeView, InfoView, SendPaymentsView, FlashView}
 
