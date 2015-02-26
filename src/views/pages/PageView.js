760b82ec92eb209a4241d83bd05aaffbb4157f2c
Adam Cmiel <adam@famo.us>
2015-02-26 11:48:03 -0800

    update transition getter

--- src/views/pages/PageView.js
+++ src/views/pages/PageView.js
@@ -2,18 +2,22 @@ import XView from '../XView'
 
 import Transitionable from 'famous/transitions/Transitionable'
 
-/*
-const transition = {
-    method: 'spring',
-    period: 800,
-    dampingRatio: 0.8
-}
-/**/
-
 const transition = {
     curve: 'easeOut',
     duration: 600
 }
+
+const noTransition = {
+    duration: 0
+}
+
+/*
+const transition = {
+    method: 'spring',
+    period: 600,
+    dampingRatio: 0.8
+}
+/**/
 
 export default class PageView extends XView {
     constructor(options) {
@@ -21,14 +25,20 @@ export default class PageView extends XView {
         this.progress = new Transitionable(1)
     }
 
+    get transition() {
+        return transition
+    }
+
     hide(cb) {
-        this.progress.set(1, transition, cb)
-    //  console.log('hide', this, transition)
+        this.progress.set(1, this.transition, () => {
+            this.progress.set(1, noTransition, cb)
+        })
     }
 
     focus(cb) {
-        this.progress.set(0, transition, cb)
-    //  console.log('focus', this, transition)
+        this.progress.set(0, this.transition, () => {
+            this.progress.set(0, noTransition, cb)
+        })
     }
 }
 
