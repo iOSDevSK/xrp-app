6a294f0c33fe026b7cd45752d627306131d0f983
Adam Cmiel <adamcmiel@gmail.com>
2015-08-22 18:41:26 +0200

    [FIX] update plugins script for modern android build

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 plugins=(
   com.phonegap.plugins.barcodescanner/
   cordova-plugin-console
@@ -8,32 +6,11 @@ plugins=(
   https://github.com/EddyVerbruggen/LaunchMyApp-PhoneGap-Plugin.git --variable URL_SCHEME=ripple
 )
 
-# pushd Build
-# 
-# for plugin in $plugins
-# do;
-# 
-# cordova plugin add $plugin
-# 
-# plugman install \
-#   --platform android \
-#   --plugin $plugin \
-#   --project .
-# 
-# done;
-# 
-# popd
-
-pushd Build-Android
+pushd Build
 
 for plugin in $plugins
-do
-
-plugman install \
-  --platform android \
-  --plugin $plugin \
-  --project .
-
+do;
+cordova plugin add $plugin
 done;
 
 popd
